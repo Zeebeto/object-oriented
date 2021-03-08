19fdeb0c0475ea6b667c301c2964bd2947c2ecee
Zeebeto <72790252+Zeebeto@users.noreply.github.com>
2021-03-08 12:55:26 +0100

    Initial commit

---
+++ Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_oriented
+{
+    class Box
+    {
+        //husk å gjøre ting public om det skal kunne brukes utenfor denne classen og .cs filen
+        public int Height;
+        public int Width;
+
+        internal void show()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace object_oriented
+{
+    class Program
+    {
+        int Width;
+        int Height;
+        static void Main(string[] args)
+        {
+            var x = new Box 
+            {
+                Width = 50,
+                Height = 30,
+            };
+            x.show();
+            Console.WriteLine(x.Height + " " + x.Width);
+        }
+    }
+}
